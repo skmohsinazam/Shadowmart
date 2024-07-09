cf99b1b199d827231a54856d75977dd055438e13
skmohsinazam <mohsin78444@gmail.com>
2024-07-09 13:00:22 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// init Isotope
+var $grid = $('.collection-list').isotope({
+  // options
+});
+// filter items on button click
+$('.filter-button-group').on( 'click', 'button', function() {
+  var filterValue = $(this).attr('data-filter');
+  resetFilterBtns();
+  $(this).addClass('active-filter-btn');
+  $grid.isotope({ filter: filterValue });
+});
+
+var filterBtns = $('.filter-button-group').find('button');
+function resetFilterBtns(){
+  filterBtns.each(function(){
+    $(this).removeClass('active-filter-btn');
+  });
+}
+//subscribeform
+function myFunction(){
+	var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+	var x=document.getElementById("Email_id");
+	if(x.value==""){
+		alert("Enter a email id...");
+	}
+	else if(x.values!="validRegex"){
+		alert("Thanku for subscribing.");
+	}
+  
+}
